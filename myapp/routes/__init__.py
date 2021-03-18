7a43456c5d320de1a93cf39cd1ca104f415c3707
RegiusPythonidae <kvara9990@yahoo.com>
2021-03-18 20:40:19 +0400

    Added determinant calculator feature

--- myapp/routes/__init__.py
+++ myapp/routes/__init__.py
@@ -1,3 +1,4 @@
 from .home import home
 from .square_number import square_number
-from .std_deviation import get_stddev
+from .std_deviation import post_stddev
+from .determinant_calculator import post_to_function
