3fd1514a1a072dc37d6b30ad1cd85a5e80c68030
Kastagnus <ucha95@gmail.com>
2021-03-19 21:04:33 +0400

    final changes

--- myapp/routes/factorial.py
+++ myapp/routes/factorial.py
@@ -1,9 +1,17 @@
 from myapp import app
 
-@app.route('/factorial/<int:n>')
-def factorial(n):
 
+def factorial(n):
     if n == 1:
         return n
     else:
-        return n*factorial(n-1)
+
+        return n * factorial(n - 1)
+
+@app.route('/factorial/<int:n>')
+def factorial_handler(n):
+    return {"result": factorial(n)}
+
+
+
+
