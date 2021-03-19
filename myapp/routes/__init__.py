5dc84ec99db06657896aa3c75f8213503190a866
Giorgi Kvaratskhelia <79371694+RegiusPythonidae@users.noreply.github.com>
2021-03-19 06:20:11 +0400

    Added determinant calculator feature (#7)
    * Added determinant calculator feature
    
    * post_to_function now returns json
    
    Co-authored-by: Temur Chichua <69351709+temurchichua@users.noreply.github.com>

--- myapp/routes/__init__.py
+++ myapp/routes/__init__.py
@@ -1,4 +1,6 @@
 from .home import home
 from .fibonacci_sequence import Fibonacci
 from .square_number import square_number
+from .std_deviation import post_stddev
+from .determinant_calculator import post_to_function
 from .std_deviation import get_stddev
